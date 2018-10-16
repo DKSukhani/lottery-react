147bd3c195b301db8d05a73cbe8d3a3858c4a644
Dipesh Sukhani <dipeshsukhani@gmail.com>
2018-10-17 01:11:32 +0800

    updated the app.js
    updated the app.js for setting the state, reading of the contract of who the manager is, updating the state and then rendering the component with that information

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import web3 from './web3';
 import lottery from './lottery'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { manager: '' };
+  }
+
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+
+    this.setState({ manager });
+  }
+
   render() {
     console.log(web3.version);
     // web3.eth.getAccounts().then(console.log);
     console.log(lottery);
 
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <p>the manager of the contract is {this.state.manager} </p>
       </div>
     );
   }
