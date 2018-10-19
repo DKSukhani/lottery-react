f1724be79aa05f6674f884391ffbd9d4d7d231e1
Dipesh Sukhani <dipeshsukhani@gmail.com>
2018-10-19 17:05:24 +0800

    Revert "Updated to push to heroku"
    This reverts commit d2b5a64013cee929d41edd911c39d2c0a43fc97f.

--- src/lottery.js
+++ src/lottery.js
@@ -1,8 +1,5 @@
 import web3 from "./web3";
-
-
-const ABI = "0x1d01A586B8B18b0643C7B8205206D46F527b23C0";
-const Address = [{ "constant": true, "inputs": [], "name": "manager", "outputs": [{ "name": "", "type": "address" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "poolSize", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [], "name": "enterLotteryContract", "outputs": [], "payable": true, "stateMutability": "payable", "type": "function" }, { "constant": false, "inputs": [], "name": "pickWinner", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "getPlayers", "outputs": [{ "name": "", "type": "address[]" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "uint256" }], "name": "players", "outputs": [{ "name": "", "type": "address" }], "payable": false, "stateMutability": "view", "type": "function" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor" }];
+import { ABI, Address } from './keySecrets';
 
 
 
